3930871abea581e3417dface59b510ae38b7ece2
聂华明 <124799379@qq.com>
2017-08-14 20:53:12 +0800

    first commit

---
+++ SwiftTeaching/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTeaching
+//
+//  Created by 聂华明 on 2017/8/14.
+//  Copyright © 2017年 shanchunchengxuyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
